e582e58a7553ede4bed00f1192018afaac16d9a0
MohamedEmara2014 <engmoh222@gmail.com>
2026-07-21 14:30:21 +0300

    main.py

--- main.py
+++ main.py
@@ -196,6 +196,8 @@ else:
                 col_configs["الحالة بالنسبة للجدول الزمني"] = st.column_config.SelectboxColumn("الحالة بالنسبة للجدول الزمني", options=TIME_STATUS_OPTIONS)
             elif sec == "التراخيص":
                 col_configs["حالة المشروع"] = st.column_config.SelectboxColumn("حالة المشروع", options=EXECUTION_STATUS_OPTIONS)
+            elif sec == "التنفيذ":
+                col_configs["حالة المشروع"] = st.column_config.SelectboxColumn("حالة المشروع", options=EXECUTION_STATUS_OPTIONS)
 
             edited_df = st.data_editor(display_df, column_config=col_configs, hide_index=True, use_container_width=True)
             
